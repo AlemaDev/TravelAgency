3140fd22ebaf2992c4d88deacab305387243ab01
alemadev <alessandromalanna@gmail.com>
2022-06-09 21:12:43 -0300

    added script to automaticaly pull changes

---
+++ up.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+function pushThis () {
+	git add .
+	echo "What will be the message?"
+	read -e message
+	git commit -m "$message"
+	echo "Are you sure you wanna push? (y/n)"
+	read -e yn
+	if [ "$yn" = y ]; then
+  		git push
+	else
+  		echo "See you!"
+	fi
+}
+
+pushThis $1
